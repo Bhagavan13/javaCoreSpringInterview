7c791c512f5d5fb09e59c1b1c036a2f9a6a6222c
Sabbisetty Bhagavan <sabbhaga@publicisgroupe.net>
2022-05-11 17:44:31 +0530

    Added the spring initial classes for the interview purpose

---
+++ src/main/java/com/bhagavan/interview/JavaCoreSpringInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.bhagavan.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreSpringInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreSpringInterviewApplication.class, args);
+	}
+
+}
